4a8f339eb40dc4028ce1c34899660964fbdd0f6d
Eric Canton <eric.d.canton@gmail.com>
2022-07-19 07:11:06 -0500

    Add streamio stuff

--- camera_picamera2.py
+++ camera_picamera2.py
@@ -1,3 +1,4 @@
+import io
 from picamera2 import Picamera2
 from base_camera import BaseCamera
 
@@ -5,9 +6,13 @@ from base_camera import BaseCamera
 class Camera(BaseCamera):
     @staticmethod
     def frames():
+        stream = io.BytesIO()
         with Picamera2() as camera:
             camera.configure(camera.create_still_configuration())
             camera.start(show_preview=False)
 
             while True:
-                yield camera.capture_image()
+                next_frame = camera.capture_image()
+                next_frame.save(stream, format='JPEG')
+                yield stream.getvalue()
+                stream.seek(0)
